d04a41c35b00cfba5d0878c13924a13b856bf626
Scott Agirs <scott@ijs.to>
2019-11-15 12:21:02 -0500

    [Add] import and declare Layout />

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import App from "next/app";
 
 import Page from "../components/Page";
+import DefaultLayout from "../layouts/DefaultLayout";
 
 class AppWrapper extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -17,6 +18,7 @@ class AppWrapper extends App {
   }
   render() {
     const { Component, pageProps } = this.props;
+    const Layout = Component.Layout || DefaultLayout;
 
     return (
       <Page>
